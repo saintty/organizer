2109803c09c27a347b2ddfa4b6debd6989e09337
Allmoon <clarthetrue@gmail.com>
2024-03-13 13:36:22 +0300

    Добавил user model + дописал .gitignore

--- backend/organizer/user/models.py
+++ backend/organizer/user/models.py
@@ -1,3 +1,10 @@
+from enum import unique
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+ 
+class User(AbstractUser):
+    name = models.CharField(max_length=10, null = False, blank = False)
+    email = models.EmailField(null = False, blank = False, unique = True, primary_key = True)
+    password = models.CharField(max_length=32, null = False, blank = False)
 
 # Create your models here.
