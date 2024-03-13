a6d70c939dba16158a0ecc1c987c056bcad2d1bd
Anton Gavrilov <anton.gavrilov.1673@mail.ru>
2024-03-13 14:05:42 +0300

    Изменен gitignor и написана модель для события

--- backend/organizer/organizer/settings.py
+++ backend/organizer/organizer/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'user',
+    'event',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -122,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = "user.User"
--- backend/organizer/user/models.py
+++ backend/organizer/user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    name = models.CharField(max_length=10, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False, unique=True, primary_key=True)
+    password = models.CharField(max_length=32, null=False, blank=False)
